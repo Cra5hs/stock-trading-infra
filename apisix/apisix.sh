094220ee651cd4bac8cee9319401dbfab909d619
cra5hs <nguyentungduong.9x1@gmail.com>
2023-08-12 22:11:31 +0700

    keycloak + apisix

--- apisix/apisix.sh
+++ apisix/apisix.sh
@@ -6,10 +6,8 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 
 helm repo update
 
-kubectl create ns ingress-apisix
+helm install apisix oci://registry-1.docker.io/bitnamicharts/apisix --namespace=stock-trad --set dashboard.enabled=false
 
-helm install apisix oci://registry-1.docker.io/bitnamicharts/apisix --namespace=ingress-apisix --set dashboard.enabled=false
-
-helm install cert-manager jetstack/cert-manager --namespace ingress-apisix  --set prometheus.enabled=false --set installCRDs=true
+helm install cert-manager jetstack/cert-manager --namespace stock-trad  --set prometheus.enabled=false --set installCRDs=true
 
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.5.0/standard-install.yaml
